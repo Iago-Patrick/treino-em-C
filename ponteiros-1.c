80317c6333e6c1a24712d98c2e7fd4562e9110aa
iago-patrick <iagopatrick14@gmail.com>
2022-11-24 21:29:18 -0300

    adicionando progs

---
+++ ponteiros-1.c
@@ -0,0 +1,33 @@
+/* --------------------------------------------------------------------------------------------- */
+/*
+compilador:
+gcc cobrinha.c -o cobrinha.x -Wall -lncurses -g -Og -lm -Wextra -ansi -pedantic-errors -DDEBUG=1
+gcc cobrinha.c -o cobrinha.exe -Wall -g -Og -lm -Wextra -ansi -pedantic-errors -DDEBUG=1*/
+/* --------------------------------------------------------------------------------------------- */
+#include <stdio.h>
+
+int main (void)
+{
+int m = 3;
+int *p1 = 0;//ponteiro
+int **p2=0;//ponteiro de ponteiro.
+
+
+printf("\n valor de M e : %i", m);
+printf("\n endereco de M e : %i", &m);
+
+// o ponteiro a posta para o endereco citado logo tbm tem o valor da variavel.
+p1 = &m;
+
+// o ponteiro de ponteiro a ponta para o endereco de um ponteiro.
+p2 = &p1;
+
+printf("\n valor de P1 e : %i", *p1);
+printf("\n o endereco que esta em P1 e : %i", p1);
+printf("\n novo valor de M e : %i", m);
+
+printf("\n valor em P2 e : %i", **p2);
+
+ return 0;
+
+}
---
+++ ponteiros-2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void soma(int *p, int a, int b);
+
+int main (void)
+{
+     int a, c, b, *p1, *p2;
+     a=4;
+    b=3;
+    p1=&a;
+    p2=p1;
+    *p2=*p1 + 3;
+    b=b *(*p1);
+
+    (*p2)++;
+
+    printf("%d %d\n", *p1, *p2);
+    printf("%d %d\n", a, b);
+    printf("c e: %d\n", c);
+    soma(&c, a, b);
+    printf("c e: %d\n", c);
+}
+
+void soma(int *p, int a, int b)
+{
+    *p = a + b;
+}
