1026dc5a895d66cac0331158be0f88908a03a13a
iago-patrick <iagopatrick14@gmail.com>
2022-12-01 23:48:17 -0300

    a dicion

---
+++ calculadora/biblio.h
@@ -0,0 +1,18 @@
+#ifndef ESTRUTURA
+#define ESTRUTURA
+
+typedef struct numero
+{
+	int x;
+	int y;
+} num;
+
+typedef struct sinal
+{
+	char x;
+	char y;
+	
+} sinal;
+
+
+#endif
---
+++ calculadora/calculadora.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "biblio.h"
+
+int main(void)
+{
+	num n;
+	sinal s;
+	
+	printf("digite dois numero entre um e outro pressione ENTER.\n");
+	scanf("%d", &n.x);
+	scanf("%d", &n.y);
+	
+	printf("digite qual operação deseja realizar entre ( +, -, /, *)\npressione ENTER.\n");
+	scanf("%d", &s.x);
+	
+	
+	return 0;
+}
+
